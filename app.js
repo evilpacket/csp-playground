9eb4ffff5cfdb5a4bf7d740bd664d56b7e38adcc
Adam Baldwin <baldwin@andyet.net>
2013-06-23 14:06:19 -0700

    piles of changes

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express'),
+    http = require('http'),
     helmet = require('helmet'),
+    fs = require('fs'),
     routes = require('./routes');
 
-var app = module.exports = express.createServer();
+var data = {
+    violation_template: "",
+    example_template: fs.readFileSync('example_template')
+};
+
+var app = express();
 
 // Configuration
 
@@ -10,7 +17,6 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
-  app.use(helmet.csp());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
@@ -24,8 +30,8 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-// Routes
 
+// Routes
 app.get('/', routes.index);
 app.get('/policy', routes.getPolicy);
 app.post('/policy', function (req, res) {
@@ -43,6 +49,30 @@ app.post('/policy', function (req, res) {
     }
 });
 
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.get('/violation', helmet.csp(), function (req, res) {
+    if (data.violation_template) {
+        res.send(data.violation_template);
+    } else {
+        res.redirect('/violation/edit');
+    }
 });
+
+
+app.get('/violation/edit', function (req, res) {
+    res.render('edit_violation', data);
+});
+
+app.post('/violation/edit', function (req, res) {
+    data.violation_template = req.body.html;
+    res.render('edit_violation', data);
+});
+
+app.post('/csp', function (req, res) {
+    console.log(req.body);
+    console.log(req.headers);
+    res.send('');
+});
+
+
+console.log('Server running on port 3000');
+var server = http.createServer(app).listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,16 @@
 var helmet = require('helmet');
 
 exports.index = function (req, res) {
-    res.render('index', { 
-        layout: false
-    });
+    res.render('index');
 };
 
 exports.getPolicy = function (req, res) {
     // Remove CSP headers for policy management page so we can't break ourselves
     res.removeHeader("X-WebKit-CSP");
+    res.removeHeader("Content-Security-Policy");
     res.removeHeader("x-content-security-policy");
     
     res.render('policy', { 
         policy: JSON.stringify(helmet.csp.toJS(), null, 4),
-        layout: false
     });
 };
